483fb2c91bf76dfb8a113ec34510aeeb3127aaa1
el486 <486.mail@gmail.com>
2015-05-21 10:20:06 -0300

    Enhanced Nominatim search

--- toolbar.js
+++ toolbar.js
@@ -272,12 +272,12 @@ var toolBarItems=[
 			// Optional options, see NominatimSearchCombo.js
 			options : {
 				zoom: 8,
-				url: 'http://open.mapquestapi.com/nominatim/v1/search.php?countrycodes=AR&addressdetails=1&format=json&limit=5',
+				url: 'http://open.mapquestapi.com/nominatim/v1/search.php?countrycodes=AR&format=json&limit=5&addressdetails=1',
 				xtype : 'hr_nominatimsearchcombo',
 				tooltip: __('Search Nominatim'),
 				id: "nominatimsearchcombo",
-				tpl: '<tpl for="."><tpl for="address"><div class="x-combo-list-item">{road} {city} {state} {postcode} {country}</div></tpl></tpl>',
-                displayTpl: '<tpl for="."><tpl for="address">{road} {city} {state} {country}</tpl></tpl>'
+				tpl: '<tpl for="."><tpl for="address"><div class="x-combo-list-item">{stream} {suburb} {state_district} {state}</div></tpl></tpl>',
+                displayTpl: '<tpl for="."><tpl for="address">{stream} {suburb} {state_district} {state}</tpl></tpl>'
 			}
 		},
 		{type: "coordinatesearch", options: {
